5cc469f458270040782e2876bfb55603daa46a4c
hyerin-Im <jeny9815@gmail.com>
2024-01-02 00:22:50 +0900

    lookbook내용추가

--- src/pages/lookbook/LookBook.jsx
+++ src/pages/lookbook/LookBook.jsx
@@ -1,16 +1,72 @@
 import { useEffect } from "react";
-import { PressWrap } from "./PressStyle";
+import { LookbookWrap } from "./LookbookStyle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+
+import "swiper/css";
 
 const LookBook = () => {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
     return (
-        <PressWrap>
-            <div className="inner">
-                <h2>LookBook 페이지 입니다</h2>
-            </div>
-        </PressWrap>
+        <LookbookWrap>
+            <Swiper
+                modules={[Navigation, Autoplay]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                autoplay={{ delay: 8000, disableOnInteraction: false }}
+                onSlideChange={() => console.log("slide change")}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <img src="./images/lookbook1.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook2.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook3.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook4.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook5.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook6.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook7.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook8.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook9.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook10.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook11.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook12.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook13.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook14.jpg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./images/lookbook15.jpg" alt="" />
+                </SwiperSlide>
+            </Swiper>
+        </LookbookWrap>
     );
 };
 
